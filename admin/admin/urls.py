76264a7277babbd803497cdd138dccc6d24bd235
jurgendn <nguyentiendung7299@gmail.com>
2020-11-29 18:46:55 +0700

    new project

--- admin/admin/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = [
-    path('dashboard/', include('dashboard.urls')),
-    path('admin/', admin.site.urls),
-]
